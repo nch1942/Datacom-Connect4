96baff1c5fb9a956f84f198db037ef9683acc04f
benjii <benjaminjameskearney@gmail.com>
2018-10-03 10:56:24 -0400

    finish session

--- DataComC4/src/datacomc4/Game.java
+++ DataComC4/src/datacomc4/Game.java
@@ -21,4 +21,8 @@ public class Game {
     public Board getBoard(){
         return board;
     }
+    
+    public byte getId(){
+        return id;
+    }
 }
--- DataComC4/src/datacomc4/Session.java
+++ DataComC4/src/datacomc4/Session.java
@@ -4,23 +4,37 @@
  * and open the template in the editor.
  */
 package datacomc4;
-
+import java.util.*;
 /**
  *
  * @author 1635547
  */
 public class Session {
-    private Game game[];
+    private List<Game> games;
     
     public Session(){
-        
+        games = new ArrayList<Game>();
     }
     
     public void createNewGame(byte gameId){
-        
+        for(int i = 0; i < games.size(); i++){
+            if(games.get(i).getId() == gameId){
+                throw new IllegalArgumentException("Game with that Id already exists!");
+            }
+        }
+        games.add(new Game(gameId));
     }
     
     public Game getGame(byte gameId){
-        return null;
+        Game game = null;
+        for(int i = 0; i < games.size(); i++){
+            if(games.get(i).getId() == gameId){
+                game = games.get(i);
+            }
+        }
+        if(game == null){
+            throw new IllegalArgumentException("Requested game does not exist!");
+        }
+        return game;
     }
 }
