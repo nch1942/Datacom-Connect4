95d4c41e0c75acce5c8afd8981ec56c12c5f8a64
Cao Hoang, Nguyen <nch1942@gmail.com>
2018-10-06 14:14:01 -0400

    create Connection method

--- DataComC4/src/datacomc4/C4Client.java
+++ DataComC4/src/datacomc4/C4Client.java
@@ -5,6 +5,8 @@
  */
 package datacomc4;
 
+import java.net.Socket;
+
 /**
  *
  * @author 1635547
@@ -12,12 +14,18 @@ package datacomc4;
 public class C4Client {
     private Player player;
     private GUI gui;
+    private Socket socket;
+    private Session session;
+    private final byte[] packet = new byte[2];
     
-    public C4Client(){
-        
+    public C4Client(Player player, GUI gui, Socket socket, byte [] packet){
+        this.player = player;
+        this.gui = gui;
+        this.socket = socket;
     }
     
     public void requestServerConnection(){
+        setPackage(packet, 0, 0);
         
     }
     
@@ -32,4 +40,12 @@ public class C4Client {
     public byte[] convertSizeOfArray(){
         return null;
     } 
+    
+    private void setPackage(byte[] packet, int offset, int value) {
+        if(packet.length != 2) {
+            throw new IllegalArgumentException("Packet Size Should be 2");
+        }
+        packet[0] = (byte)offset;
+        packet[1] = (byte)value;
+    }
 }
